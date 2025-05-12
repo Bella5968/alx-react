891adbdf6142e227fc5c9b765e7cbc786ecb4ee7
bella5968 <abitimenge@gmail.com>
2025-05-12 20:13:38 +0000

    react

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,38 +1,52 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: {
-    main: path.resolve(__dirname, "./js/dashboard_main.js"),
-  },
+  entry: './js/dashboard_main.js', // Entry point for your JavaScript
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    filename: 'bundle.js',  // Output the bundled file as "bundle.js"
+    path: path.resolve(__dirname, 'public'),  // Output directory
+    clean: true  // Clean the build folder on each build
   },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
+  mode: 'production',  // Set mode to production
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/,  // Use CSS loader for CSS files
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(gif|png|jp?g|svg)$/i,
+        test: /\.(png|jpe?g|gif)$/i,  // For handling images (like jpg, png)
         use: [
-          "file-loader",
           {
-            loader: "image-webpack-loader",
+            loader: 'file-loader',
             options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
+              name: '[name].[ext]',
+              outputPath: 'assets/',  // Output images to "assets" folder
+            }
           },
         ],
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ALX Dashboard',
+      template: './public/index.html',  // Use this as the template for the HTML
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ['imagemin-mozjpeg', { quality: 70 }],
+          ['imagemin-pngquant', { quality: [0.65, 0.90], speed: 4 }],
+        ],
+      },
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
